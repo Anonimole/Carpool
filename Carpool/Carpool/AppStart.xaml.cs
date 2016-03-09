61d7efd135a5444786a91750c4b74973507e83bc
Luis Manuel Rodriguez Scio PC <lwe_@hotmail.com>
2016-03-09 16:41:04 -0600

    Login View

---
+++ Carpool/Carpool/AppStart.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Carpool
+{
+	public partial class AppStart : Application
+	{
+		public AppStart ()
+		{
+			InitializeComponent ();
+            MainPage = new Login();
+		}
+	}
+}
