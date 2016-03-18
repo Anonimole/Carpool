6a44f04348f3d8fc7d628e378c12274d4b752845
Luis Manuel Rodriguez Scio PC <lwe_@hotmail.com>
2016-03-18 10:02:01 -0600

    Profile view Modified

--- Carpool/Carpool/Views/Profile.xaml.cs
+++ Carpool/Carpool/Views/Profile.xaml.cs
@@ -3,21 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using  System.Diagnostics;
 
 using Xamarin.Forms;
+using XLabs.Forms.Controls;
 
 namespace Carpool
 {
 	public partial class Profile : ContentPage
 	{
-		public Profile ()
-		{
+        private string genderSelected;
+
+        public Profile ()
+        {
+	        genderSelected = "";
+            
 			InitializeComponent ();
-		}
+
+            genderPicker.SelectedIndexChanged += (sender, args) =>
+            {
+                if (genderPicker.SelectedIndex == -1)
+                {
+                    genderSelected="";
+                }
+                else
+                {
+                    genderSelected = genderPicker.Items[genderPicker.SelectedIndex];
+                    genderPicker.BackgroundColor = Color.FromHex("#004D40");
+
+                }
+            };
+            
+        }
 
         async void Dashboard(object sender, EventArgs e)
         {
-            Application.Current.MainPage = new NavigationPage(new Dashboard());
+
+            string name = this.nameEntry.Text;
+            string age = this.ageEntry.Text;
+            string phone = this.phoneEntry.Text;
+
+	        
+            Debug.WriteLine("{0} {1} {2} {3} " ,name,age,phone,genderSelected);
+
+            //Application.Current.MainPage = new NavigationPage(new Dashboard());
         }
 	}
 }
