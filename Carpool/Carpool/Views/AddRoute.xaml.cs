d3009a6ec870ef1d8cdf9305bed3b60c69213a4f
Luis Manuel Rodriguez Scio PC <lwe_@hotmail.com>
2016-03-23 17:03:11 -0600

    Add cars

--- Carpool/Carpool/Views/AddRoute.xaml.cs
+++ Carpool/Carpool/Views/AddRoute.xaml.cs
@@ -4,42 +4,42 @@ using Xamarin.Forms;
 
 namespace Carpool
 {
-	public partial class AddRoute : ContentPage
-	{
-	    private CarsManager carsManager;
-	    private Users currentUser;
+    public partial class AddRoute : ContentPage
+    {
+        private CarsManager carsManager;
+        private Users currentUser;
 
-		public AddRoute ()
-		{
-            carsManager= new CarsManager();
-            currentUser=(Users) Application.Current.Properties["user"];
-		    this.IsBusy = true;
-			InitializeComponent ();
-            
-		}
+        public AddRoute()
+        {
+            carsManager = new CarsManager();
+            currentUser = (Users)Application.Current.Properties["user"];
+            this.IsBusy = true;
+            InitializeComponent();
 
-	    async void OnAdd(object sender, EventArgs e)
-	    {
-	        await Navigation.PushAsync(new AddCar());
-	    }
+        }
 
-	    protected override async void OnAppearing()
-	    {
-	        base.OnAppearing();
+        async void OnAdd(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddCar());
+        }
 
-	        var carsList = await carsManager.GetMyCarsAsync(currentUser);
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var carsList = await carsManager.GetMyCarsAsync(currentUser);
 
             carPicker.Items.Clear();
 
-	        foreach (var car in carsList)
-	        {
-	            carPicker.Items.Add(car.Model);
-	        }
+            foreach (var car in carsList)
+            {
+                carPicker.Items.Add(car.Model);
+            }
 
-	        carPicker.IsEnabled = carPicker.Items.Count > 0 ? true : false;
+            carPicker.IsEnabled = carPicker.Items.Count > 0 ? true : false;
 
-	        this.IsBusy = false;
+            this.IsBusy = false;
 
-	    }
-	}
+        }
+    }
 }
