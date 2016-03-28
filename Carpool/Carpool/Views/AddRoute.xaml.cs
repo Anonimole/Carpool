ebead942ef9f3fd8f1200a599d2778324f50b951
Luis Manuel Rodriguez <lwe_@hotmail.com>
2016-03-27 20:05:32 -0600

    Add route functionality

--- Carpool/Carpool/Views/AddRoute.xaml.cs
+++ Carpool/Carpool/Views/AddRoute.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using Xamarin.Forms;
 
@@ -8,15 +9,19 @@ namespace Carpool
     {
         private CarsManager carsManager;
         private Users currentUser;
+        private Route newRoute;
         private bool carSelected;
+        private IDictionary<string,object> properties;
 
         public AddRoute()
         {
             carSelected = false;
             carsManager = new CarsManager();
+            properties = Application.Current.Properties;
             currentUser = (Users)Application.Current.Properties["user"];
             this.IsBusy = true;
             InitializeComponent();
+            
         }
 
         async void OnAdd(object sender, EventArgs e)
@@ -45,12 +50,24 @@ namespace Carpool
                 this.IsBusy = false;
             }
 
+            if (properties.ContainsKey("route"))
+            {
+                newRoute = (Route)Application.Current.Properties["route"];
+
+                if(!string.IsNullOrEmpty(newRoute.From_Latitude))
+                startingPointButton.Text = "Change Starting point";
+            }
         }
 
 
         public async void OnStartingPoint(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new MapStartingPoint());
+        }
+
+        public async void OnEndingPoint(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MapEndingPoint());
         }
 
         public void OnCarPicker(object sender, EventArgs e)
